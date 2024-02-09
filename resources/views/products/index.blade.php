e4979f5eb3969751c47de797950f717536159dc0
Zignuts Parth Gupta <parthg@zignuts.com>
2024-02-09 11:52:56 +0530

    added the sweet alert on delete button in index page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,27 +11,33 @@
 
     <h1 class="text-center ">All Blogs</h1>
 
-      <div class="container mt-4">
-        <div class="row justify-content-center">
-            {{-- feteching data --}}
-            @foreach($products as $product)
-            <div class="col-md-4 mb-4">
-                <div class="card" >
-                    <div class="card-body">
-                        <a href="products/{{$product->id}}/show" class="text-dark"> <h5 class="card-title">{{ $product->name }}</h5> </a>
-                        <p class="card-text">{{ \Illuminate\Support\Str::limit($product->description, 20, '...') }}</p>
-                        <a href="products/{{$product->id}}/edit" class="btn btn-primary">Edit</a>
-                        <a href="products/{{$product->id}}/delete" class="btn btn-danger" onclick="return confirm('Are You Sure You Want To Delete ?')">Delete</a>
+
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                {{-- fetching data --}}
+                @foreach($products as $product)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="products/{{$product->id}}/show" class="text-dark"> <h5 class="card-title">{{ $product->name }}</h5> </a>
+                            <p class="card-text">{{ \Illuminate\Support\Str::limit($product->description, 20, '...') }}</p>
+                            <a href="products/{{$product->id}}/edit" class="btn btn-primary">Edit</a>
+                            <button class="btn btn-danger delete-product" data-id="{{ $product->id }}">Delete</button>
+                        </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
+            {{-- pagination --}}
+            <div class="mx-auto">
+                {{$products->links()}}
+            </div>
         </div>
+        
+ 
+        
 
-        {{-- pagination --}}
-        <div class="mx-auto">
-            {{$products->links()}}
-        </div>
+        
     </div>
 
 @endsection
